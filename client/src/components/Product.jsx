e2f0a898c5f9a0e5b1574d37ecdca743305630be
Satish9047 <satishprajapati930@gmail.com>
2024-03-26 13:48:10 +0545

    07: add ellipsis to product title + add rating in product component

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,6 +1,8 @@
 import { Card } from "react-bootstrap";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3  p-3  rounded">
@@ -10,10 +12,17 @@ const Product = ({ product }) => {
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
@@ -27,6 +36,8 @@ Product.propTypes = {
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    numReviews: PropTypes.number.isRequired,
   }).isRequired,
 };
 
