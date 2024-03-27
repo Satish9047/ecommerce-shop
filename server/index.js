27de3b85989201bde23a7d2e22ab075ed56fd144
Satish9047 <satishprajapati930@gmail.com>
2024-03-27 09:12:00 +0545

    11: add concurrently + nodemon

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import products from "./data/products.js";
 
 const app = express();
 
@@ -6,8 +7,16 @@ const PORT = 8080;
 
 app.get("/", (req, res) => {
     res.send("API is running");
-})
+});
+
+app.get("/api/products", (req, res) => {
+    res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on  port ${PORT}`);
-})
+});
