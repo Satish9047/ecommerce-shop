7fa92e34673bef2938d4ba1c7d12e5a50208aba6
Satish9047 <satishprajapati930@gmail.com>
2024-03-30 11:01:55 +0545

    23: error handler bug fixed

--- server/src/middlewares/error.js
+++ server/src/middlewares/error.js
@@ -1,10 +1,11 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.origialUrl}`);
+    const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
 };
 
-const errorHandler = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, _req, res, _next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
@@ -16,7 +17,7 @@ const errorHandler = (err, req, res) => {
 
     }
 
-    res.status(statusCode).jsonn({
+    res.status(statusCode).json({
         message,
         stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack
     });
