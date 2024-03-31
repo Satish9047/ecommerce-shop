147f5631be741165bf70157e38ae9aa30fa8bcf1
Satish9047 <satishprajapati930@gmail.com>
2024-03-31 10:30:25 +0545

    24: minor change in product router

--- server/src/Routes/product.js
+++ server/src/Routes/product.js
@@ -1,24 +1,8 @@
 import express from "express";
 const productRouter = express.Router();
+import { getProducts, getProductById } from "../Controllers/products.js";
 
-import asyncHandler from "../middlewares/asyncHandler.js";
-import Product from "../models/products.js";
-
-productRouter.get("/", asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    console.log(products, "these are the product from database");
-    res.json(products);
-
-}));
-
-productRouter.get("/:id", asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-        return res.json(product);
-    } else {
-        res.status(404);
-        throw new Error("Resource not Found");
-    }
-}));
+productRouter.get("/", getProducts);
+productRouter.get("/:id", getProductById);
 
 export default productRouter;
