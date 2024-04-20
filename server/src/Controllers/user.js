cad114ca0a7d2c054e8e80ee6ab163b868b13127
Satish9047 <satishprajapati930@gmail.com>
2024-04-20 20:01:26 +0545

    41: create generateToken function in the utils folder to use DRY principle

--- server/src/Controllers/user.js
+++ server/src/Controllers/user.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/user.js";
-import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
 
 /**
  * @desc    Auth user & get token
@@ -12,17 +12,8 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
+    generateToken(res, user.id);
 
-    // set JWT as http-only cookie
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000, //1day
-    });
     res.json({
       _id: user.id,
       name: user.name,
@@ -57,6 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user.id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
