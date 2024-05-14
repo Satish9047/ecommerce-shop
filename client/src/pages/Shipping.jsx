262f206a8a4e0be87f83668ed08a6ef6bf8e48d4
Satish9047 <satishprajapati930@gmail.com>
2024-05-14 18:29:29 +0545

    65: add shipping step and ordering step navbar at shipping

--- client/src/pages/Shipping.jsx
+++ client/src/pages/Shipping.jsx
@@ -4,6 +4,7 @@ import FormContainer from "../components/FormContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "../slices/cartSlice";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const Shipping = () => {
   const cart = useSelector((state) => state.cart);
@@ -27,6 +28,7 @@ const Shipping = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address" className="my-2">
