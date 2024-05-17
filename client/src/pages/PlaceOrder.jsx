c410bb473a662ac5413836cfc001bc021c5ff392
Satish9047 <satishprajapati930@gmail.com>
2024-05-17 22:02:50 +0545

    74: update placeOrder's page UI

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -1,10 +1,16 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import CheckoutSteps from "../components/CheckoutSteps";
+// import { Toast } from "react-toastify";
+import { useCreatOrderMutation } from "../slices/orderApiSlice";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { clearCartItems } from "../slices/cartSlice";
 
 const PlaceOrder = () => {
+  //   const dispatch = useDispatch();
   const navigate = useNavigate();
   const cart = useSelector((state) => state.cart);
 
@@ -20,7 +26,49 @@ const PlaceOrder = () => {
     <>
       <CheckoutSteps step1 step2 step3 step4 />
       <Row>
-        <Col md={8}>Column</Col>
+        <Col md={8}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},{" "}
+                {cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method:</strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <ListGroup>
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
         <Col md={4}>Column</Col>
       </Row>
     </>
