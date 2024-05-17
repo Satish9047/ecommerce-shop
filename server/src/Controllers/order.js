663f7d491673c4485dd7234df169d67dcf2d4112
Satish9047 <satishprajapati930@gmail.com>
2024-05-17 21:19:38 +0545

    69: update create order items controller

--- server/src/Controllers/order.js
+++ server/src/Controllers/order.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
-// import Order from "../models/order.js";
+import Order from "../models/order.js";
 
 /**
  * @description     Create new order
@@ -8,7 +8,36 @@ import asyncHandler from "../middlewares/asyncHandler.js";
  *
  */
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+  if (orderItems && orderItems.length == 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = await Order.create({
+      orderItems: orderItems.map((item) => ({
+        ...item,
+        product: item._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 /**
