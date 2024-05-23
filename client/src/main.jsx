301cd956c554645a8a41cf7637e21ed6d27c574e
Satish9047 <satishprajapati930@gmail.com>
2024-05-23 09:02:46 +0545

    85: update Order page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,6 +16,7 @@ import Shipping from "./pages/Shipping.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import Payment from "./pages/Payment.jsx";
 import PlaceOrder from "./pages/PlaceOrder.jsx";
+import Order from "./pages/Order.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -32,6 +33,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
               <Route path="/shipping" element={<Shipping />} />
               <Route path="/payment" element={<Payment />} />
               <Route path="/placeorder" element={<PlaceOrder />} />
+              <Route path="/order/:id" element={<Order />} />
             </Route>
           </Route>
         </Routes>
---
+++ client/src/pages/Order.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+// import {
+//   Row,
+//   Col,
+//   ListGroup,
+//   Image,
+//   Form,
+//   Button,
+//   Card,
+// } from "react-bootstrap";
+// import Message from "../components/Message";
+// import Loader from "../components/Loader";
+import { useGetOrderDetailsQuery } from "../slices/orderApiSlice";
+
+const Order = () => {
+  const { id: orderId } = useParams();
+  const {
+    data: order,
+    refetch,
+    isLoading,
+    error,
+  } = useGetOrderDetailsQuery(orderId);
+
+  //   console.log(order);
+
+  return <div>order page</div>;
+};
+
+export default Order;
