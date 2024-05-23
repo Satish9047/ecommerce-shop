8b6bb41560b9ff38c017ae6e7d5e5d3fbed1a89f
Satish9047 <satishprajapati930@gmail.com>
2024-05-23 08:26:52 +0545

    82: fixed the itemsPrice not getting value

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -30,7 +30,7 @@ const PlaceOrder = () => {
         orderItems: cart.cartItems,
         shippingAddress: cart.shippingAddress,
         paymentMethod: cart.paymentMethod,
-        itemsPrice: cart.cartItemsPrice,
+        itemsPrice: cart.itemPrice,
         shippingPrice: cart.shippingPrice,
         taxPrice: cart.taxPrice,
         totalPrice: cart.totalPrice,
