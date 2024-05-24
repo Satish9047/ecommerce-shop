e140c64a9c15006ae80999088ef25979f5dec99b
Satish9047 <satishprajapati930@gmail.com>
2024-05-24 21:27:36 +0545

    88: add Order UI on the left side for the page

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -1,15 +1,15 @@
-import { useParams } from "react-router-dom";
-// import {
-//   Row,
-//   Col,
-//   ListGroup,
-//   Image,
-//   Form,
-//   Button,
-//   Card,
-// } from "react-bootstrap";
-// import Message from "../components/Message";
-// import Loader from "../components/Loader";
+import { Link, useParams } from "react-router-dom";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 import { useGetOrderDetailsQuery } from "../slices/orderApiSlice";
 
 const Order = () => {
@@ -21,9 +21,81 @@ const Order = () => {
     error,
   } = useGetOrderDetailsQuery(orderId);
 
-  console.log(order);
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger" />
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Name:</strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong> {order.user.email}
+              </p>
+              <p>
+                <strong>Address:</strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city},{" "}
+                {order.shippingAddress.postalCode},{" "}
+                {order.shippingAddress.country}
+              </p>
+              <p>
+                {order.isDelivered ? (
+                  <Message variant="success">
+                    Delivered on {order.deliveredAt}
+                  </Message>
+                ) : (
+                  <Message variant="danger">Not Delivered</Message>
+                )}
+              </p>
+            </ListGroup.Item>
 
-  return <div>order page</div>;
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method:</strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant={"success"}>Paid</Message>
+              ) : (
+                <Message variant={"danger"}>Not Paid</Message>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.orderItems.map((item, index) => {
+                return (
+                  <ListGroup.Item key={index}>
+                    <Row>
+                      <Col md={1}>
+                        <Image src={item.image} alt={item.name} fluid rounded />
+                      </Col>
+                      <Col>
+                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                      </Col>
+                      <Col md={4}>
+                        {item.qty} x ${item.price} = ${item.qty * item.price}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>Column</Col>
+      </Row>
+    </>
+  );
+
+  // return <div>order page</div>;
 };
 
 export default Order;
