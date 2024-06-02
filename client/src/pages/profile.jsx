ae3475b86d8508839e0bca6ff2ef5f81256c05fd
Satish9047 <satishprajapati930@gmail.com>
2024-06-02 22:12:06 +0545

    105: update profile page UI + Form

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,7 +1,89 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Table, Row, Col, Button, Form } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { usePorfileMutation } from "../slices/userApiSlice";
+import { toast } from "react-toastify";
+import { setCredentials } from "../slices/authSlice";
 
 const profile = () => {
-  return <div>profile page</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { userInfo } = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (userInfo) {
+      setName(userInfo.name);
+      setEmail(userInfo.email);
+    }
+  }, [userInfo.name, userInfo.email]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submitHandler");
+  };
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name" className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="email" className="my-2">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password" className="my-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Form>
+
+        <Form.Group controlId="confirmPassword" className="my-2">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="Password"
+            placeholder="Re-Enter Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="my-2">
+          Update
+        </Button>
+      </Col>
+      <Col md={9}>Column 2</Col>
+    </Row>
+  );
 };
 
 export default profile;
